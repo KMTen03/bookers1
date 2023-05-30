f33f3ae58c6dc2b02e15fe8df17936a472e58f39
KMTen03 <129754107+KMTen03@users.noreply.github.com>
2023-05-30 14:14:01 +0000

    task02

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -16,28 +17,24 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:success] = "Book was successfully created."
-      redirect_to '/books'
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
-      flash[:success] = "error"
       render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
-
   end
 
   def update
     @book = Book.find(params[:id])
-    book = Book.update(book_params)
-    if book.update
+    if @book.update(book_params)
       flash[:success] = "Book was successfully updated."
-      redirect_to '/books'
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
-      flash[:success] = "error"
       render :index
     end
   end
